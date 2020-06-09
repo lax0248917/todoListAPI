112c871ceebd43eea3fb8278157d2f791b430b65
Zachary King <lax0248917@gmail.com>
2020-06-09 17:22:26 -0400

    :rocket: update to server.js file

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-  res.status(404).send({
-    url: req.originalUrl + ' not found'
-  })
-});
 
 const routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
@@ -29,3 +24,9 @@ app.listen(port);
 
 
 console.log('todo list RESTful API server started on: ' + port);
+
+app.use(function (req, res) {
+  res.status(404).send({
+    url: req.originalUrl + ' not found'
+  })
+});
